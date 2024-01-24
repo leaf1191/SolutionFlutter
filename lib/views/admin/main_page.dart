f2d17f0239262422ff4dbc63b605a47504e29fe3
leaf1191 <jwlv111@naver.com>
2024-01-25 00:58:38 +0900

    grid 기본 틀 구현

--- lib/views/admin/main_page.dart
+++ lib/views/admin/main_page.dart
@@ -11,9 +11,26 @@ class MainPage extends StatelessWidget {
       onWillPop: context.read<AdminViewModel>().exitTapTwice,
       child: Scaffold(
         body: SafeArea(
-          child: IconButton(
-            onPressed: () { Navigator.pushNamed(context, '/admin/register'); },
-            icon: const Icon(Icons.add_box_outlined),
+          child: Column(
+            children: [
+              IconButton(
+                onPressed: () { Navigator.pushNamed(context, '/admin/register'); },
+                icon: const Icon(Icons.add_box_outlined),
+              ),
+              Expanded(
+                child: GridView.builder(
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3,mainAxisExtent: 180),
+                    itemCount: 10,
+                    itemBuilder: (context, i){
+                      return Container(
+                        margin: const EdgeInsets.all(5),
+                        color: Colors.blue,
+                        child: Center(child: Text('$i')),
+                      );
+                    }
+                ),
+              ),
+            ],
           ),
         ),
       ),
