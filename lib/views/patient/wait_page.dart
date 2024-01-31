12b94cbae3212d1be7e6cedd088c1ce946014a02
leaf1191 <jwlv111@naver.com>
2024-02-01 01:52:30 +0900

    대기 페이지 초안 완성

--- lib/views/patient/wait_page.dart
+++ lib/views/patient/wait_page.dart
@@ -1,5 +1,6 @@
 import 'package:emergency_mate/colors/colors.dart';
 import 'package:emergency_mate/widgets/app_bar.dart';
+import 'package:emergency_mate/widgets/basic_button.dart';
 import 'package:flutter/material.dart';
 
 class WaitPage extends StatelessWidget {
@@ -7,11 +8,12 @@ class WaitPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    bool value = true;
+    return Scaffold(
       body: SafeArea(
         child: Column(
-          children: [
-            SizedBox(
+          children: value? [
+            const SizedBox(
               height: 60,
               child: BackAppBar(
                 back: 'Home',
@@ -19,6 +21,176 @@ class WaitPage extends StatelessWidget {
                 color: MAIN_COLOR,
               ),
             ),
+            Expanded(flex: 4,child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 30),
+                    child: const Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Text('실시간 대기',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),)
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 4,
+                  child: Container(
+                    margin: const EdgeInsets.all(30),
+                    child: BasicButton(
+                      width: double.infinity,
+                      height: double.infinity,
+                      color: MAIN_COLOR,
+                      child: Column(
+                        children: [
+                          Expanded(
+                              flex: 2,
+                              child: Container(
+                                  margin: const EdgeInsets.all(20),
+                                  child: Image.asset('assets/wait/user_img.png')
+                              ),
+                          ),
+                          const Expanded(
+                              child: Text('10번',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),)
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )),
+            Expanded(
+                flex: 5,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 30),
+                        child: const Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Text('내 정보',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),)
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 10,
+                      child: Container(
+                        margin: const EdgeInsets.all(30),
+                        child: BasicButton(
+                          width: double.infinity,
+                          height: double.infinity,
+                          color: MAIN_COLOR,
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(vertical: 10),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Expanded(child: Container(
+                                      margin: const EdgeInsets.symmetric(horizontal: 20),
+                                      child: const Row(
+                                        children: [
+                                          Text('이름',style: TextStyle(color: MAIN_COLOR,fontWeight: FontWeight.bold,fontSize: 20),),
+                                          SizedBox(width: 5,),
+                                          Text('리얼 이름',style: TextStyle(fontWeight: FontWeight.bold),),
+                                        ],
+                                      ),
+                                    ),),
+                                Expanded(child: Container(
+                                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                                  child: const Row(
+                                    children: [
+                                      Text('생년월일',style: TextStyle(color: MAIN_COLOR,fontWeight: FontWeight.bold,fontSize: 20),),
+                                      SizedBox(width: 5,),
+                                      Text('24.02.22',style: TextStyle(fontWeight: FontWeight.bold),),
+                                    ],
+                                  ),
+                                )),
+                                Expanded(child: Container(
+                                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                                  child: const Row(
+                                    children: [
+                                      Text('증상',style: TextStyle(color: MAIN_COLOR,fontWeight: FontWeight.bold,fontSize: 20),),
+                                      SizedBox(width: 5,),
+                                      Text('내 폰 기준 17자 넘으면 깨짐',style: TextStyle(fontWeight: FontWeight.bold),),
+                                    ],
+                                  ),
+                                )),
+                                Expanded(child: Container(
+                                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                                  child: const Text('연락처',style: TextStyle(color: MAIN_COLOR,fontWeight: FontWeight.bold,fontSize: 20),),
+                                )),
+                                Expanded(child: Container(
+                                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                                  child: const Text('010-1234-5678',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                                )),
+                                Expanded(child: Container(
+                                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                                  child: const Text('보호자 연락처',style: TextStyle(color: MAIN_COLOR,fontWeight: FontWeight.bold,fontSize: 20),),
+                                )),
+                                Expanded(child: Container(
+                                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                                  child: const Text('010-1234-5678',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                                )),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+            ),
+          ] : [
+            const SizedBox(
+              height: 60,
+              child: BackAppBar(
+                back: 'Home',
+                title: '대기 확인',
+                color: MAIN_COLOR,
+              ),
+            ),
+            Expanded(flex: 4,child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 30),
+                    child: const Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Text('실시간 대기',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),)
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 4,
+                  child: Container(
+                    margin: const EdgeInsets.all(30),
+                    child: BasicButton(
+                      width: double.infinity,
+                      height: double.infinity,
+                      color: MAIN_COLOR,
+                      child: Column(
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: Container(
+                                margin: const EdgeInsets.all(20),
+                                child: Image.asset('assets/wait/user_img.png')
+                            ),
+                          ),
+                          const Expanded(
+                              child: Text('대기 정보 없음',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),)
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )),
+            const Expanded(flex: 5,child: SizedBox(),)
           ],
         ),
       ),
