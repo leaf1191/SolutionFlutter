f756b9b79969e43ca72bad4ffa9b84c311d49e56
leaf1191 <jwlv111@naver.com>
2024-02-02 22:22:55 +0900

    로그인 페이지 디자인 완성 (2안)

--- lib/views/intro/title_page.dart
+++ lib/views/intro/title_page.dart
@@ -1,3 +1,4 @@
+import 'package:emergency_mate/colors/colors.dart';
 import 'package:emergency_mate/firebase/auth/firebase_auth.dart';
 import 'package:emergency_mate/viewmodels/intro/intro_viewmodel.dart';
 import 'package:flutter/material.dart';
@@ -9,24 +10,68 @@ class TitlePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const double margin = 80;
+    const double circular = 120;
+
     return WillPopScope(
       onWillPop: context.read<IntroViewModel>().exitTapTwice,
       child: Scaffold(
+        backgroundColor: Colors.blue.shade100,
         body: SafeArea(
-          child: Center(
-            child: ElevatedButton(
-              onPressed: () async {
-                try{
-                  await auth.signInWithGoogle();
-                  // ignore: use_build_context_synchronously
-                  Navigator.pushNamed(context, '/intro/select');
-                } catch(e){
-                  Fluttertoast.showToast(msg: '로그인 실패');
-                }
-              },
-              child: const Text('구글 로그인'),
-            ),
-          ),
+          child: Column(
+            children: [
+              Expanded(child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  margin: const EdgeInsets.fromLTRB(margin, margin, margin, 0),
+                  child: Image.asset('assets/logo.png'),
+                ),
+              )),
+              Expanded(child: Container(
+                margin: const EdgeInsets.all(30),
+                child: const Text('Welcome',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+              )),
+              Expanded(child: Container(
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topRight: Radius.circular(circular),topLeft: Radius.circular(circular))
+                ),
+                child: Column(
+                  children: [
+                    const Expanded(
+                        child: Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Text('Login',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),)
+                        ),
+                    ),
+                    Expanded(flex: 2,child: Align(
+                      child: GestureDetector(
+                        onTap: () async {
+                          try{
+                            await auth.signInWithGoogle();
+                            // ignore: use_build_context_synchronously
+                            Navigator.pushNamed(context, '/intro/select');
+                          } catch(e){
+                            Fluttertoast.showToast(msg: '로그인 실패');
+                          }
+                        },
+                        child: Container(
+                          width: 250,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: MAIN_COLOR),
+                            borderRadius: BorderRadius.circular(50)
+                          ),
+                          child: const Center(child: Text('Google Login',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25,color: MAIN_COLOR),)),
+                        ),
+                      ),
+                    )),
+                  ],
+                ),
+              )),
+            ],
+          )
         ),
       ),
     );
