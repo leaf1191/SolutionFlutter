9742c528f88cb76ea963525e4f41dda34dfa8e1e
leaf1191 <jwlv111@naver.com>
2024-02-06 17:18:42 +0900

    역할 유형 선택 페이지 완성

--- lib/views/intro/select_page.dart
+++ lib/views/intro/select_page.dart
@@ -1,7 +1,10 @@
+import 'package:emergency_mate/colors/colors.dart';
 import 'package:emergency_mate/firebase/auth/firebase_auth.dart';
 import 'package:emergency_mate/http/dio.dart';
 import 'package:emergency_mate/models/network_model.dart';
 import 'package:emergency_mate/viewmodels/intro/intro_viewmodel.dart';
+import 'package:emergency_mate/widgets/app_bar.dart';
+import 'package:emergency_mate/widgets/basic_button.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provider/provider.dart';
@@ -17,14 +20,22 @@ class SelectPage extends StatelessWidget {
       onWillPop: () async => false,
       child: Scaffold(
         body: SafeArea(
-          child: SizedBox(
-            height: double.infinity,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
+          child: Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/background_img.jpg'),
+                    fit: BoxFit.fill
+                )
+            ),
+            child: Column(
               children: [
-                ElevatedButton(
-                    onPressed: () async {
+                const SizedBox(height: 60,child: BasicAppBar(
+                  title: '유형 선택',
+                )),
+                const Expanded(child: Align(alignment: Alignment.bottomCenter,child: Text('이용하시는 역할 유형을\n        선택해주세요.',style: TextStyle(fontSize: 20),))),
+                Expanded(flex: 3,
+                  child: GestureDetector(
+                    onTap: () async {
                       try{
                         await introViewModel.chooseUserRole(Role.patient);
                         // ignore: use_build_context_synchronously
@@ -33,10 +44,27 @@ class SelectPage extends StatelessWidget {
                         Fluttertoast.showToast(msg: '선택하는데 실패하였습니다.');
                       }
                     },
-                    child: const Text('환자')
-                ),
-                ElevatedButton(
-                    onPressed: () async{
+                    child: Container(
+                        margin: const EdgeInsets.all(30),
+                        child: BasicButton(
+                          width: double.infinity,
+                          height: double.infinity,
+                          color: MAIN_COLOR,
+                          child: Column(
+                            children: [
+                              Expanded(flex: 3,child: Container(
+                                margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                                child: Image.asset('assets/select/patient.png'),
+                              )),
+                              const Expanded(flex: 2,child: Text('환자',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: MAIN_COLOR),))
+                            ],
+                          ),
+                        ),
+                                    ),
+                  ),),
+                Expanded(flex: 3,
+                  child: GestureDetector(
+                    onTap: () async {
                       try{
                         await introViewModel.chooseUserRole(Role.admin);
                         // ignore: use_build_context_synchronously
@@ -45,8 +73,25 @@ class SelectPage extends StatelessWidget {
                         Fluttertoast.showToast(msg: '선택하는데 실패하였습니다.');
                       }
                     },
-                    child: const Text('관리')
-                )
+                    child: Container(
+                    margin: const EdgeInsets.all(30),
+                    child: BasicButton(
+                      width: double.infinity,
+                      height: double.infinity,
+                      color: MAIN_COLOR,
+                      child: Column(
+                        children: [
+                          Expanded(flex: 3,child: Container(
+                            margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                            child: Image.asset('assets/select/admin.png'),
+                          )),
+                          const Expanded(flex: 2,child: Text('관리자',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: MAIN_COLOR),))
+                        ],
+                      ),
+                    ),
+                                    ),
+                  ),),
+                const Expanded(child: SizedBox()), // 비율 조정
               ],
             ),
           ),
@@ -55,3 +100,4 @@ class SelectPage extends StatelessWidget {
     );
   }
 }
+
